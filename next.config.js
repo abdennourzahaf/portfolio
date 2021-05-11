7992f288384f918e5a55873df95b2707df29619c
Abdennour <zfnori@gmail.com>
2021-05-11 22:31:49 +0100

    removed inline images

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 const withOptimizedImages = require('next-optimized-images');
 
-module.exports = withOptimizedImages({});
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer(
+  withOptimizedImages({
+    inlineImageLimit: -1,
+  }),
+);
