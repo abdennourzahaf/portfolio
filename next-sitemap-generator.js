b110e48febea61a5458dc233340210d44074b684
Abdennour <zfnori@gmail.com>
2021-05-21 02:38:41 +0100

    fixed sitemap generator

--- next-sitemap-generator.js
+++ next-sitemap-generator.js
@@ -5,7 +5,7 @@ sitemap({
   ignoredPaths: ['assets', 'admin'],
   ignoreIndexFiles: true,
   ignoredExtensions: ['png', 'xml', 'ico', 'webmanifest', 'svg'],
-  pagesDirectory: __dirname + '\\out',
+  pagesDirectory: __dirname + '/out',
   targetDirectory: 'out/',
-  nextConfigPath: __dirname + '\\next.config.js',
+  nextConfigPath: __dirname + '/next.config.js',
 });
