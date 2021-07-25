47917cbda5b4d0e43fd18ea2eeae6b1b58114d60
Abdennour Zahaf <zfnori@gmail.com>
2021-07-25 15:24:02 +0100

    improved styling

--- components/SectionHeader/SectionHeader.jsx
+++ components/SectionHeader/SectionHeader.jsx
@@ -6,7 +6,7 @@ const SectionHeader = ({ title, primary, secondary }) => {
       <h2 className='text-[1.2rem] font-bold uppercase text-primary mb-4 tracking-[4px]'>
         {title}
       </h2>
-      <h3 className='text-text-dark capitalize font-bold text-[2.2rem] max-w-[480px] mx-auto mb-4'>
+      <h3 className='text-text-dark capitalize font-bold text-[2.2rem] max-w-[480px] mx-auto mb-1'>
         {primary}
       </h3>
       <p className='text-text max-w-[520px] mx-auto'>{secondary}</p>
