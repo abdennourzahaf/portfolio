70734162e023da8dc64f1c3830878c9301027aca
Abdennour Zahaf <zfnori@gmail.com>
2022-02-21 23:24:32 +0100

    added link to upwork

--- containers/Contact/Contact.jsx
+++ containers/Contact/Contact.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 import Button from '../../components/Button/Button';
 import OptimizedImage from '../../components/OptimizedImage/OptimizedImage';
 import EmailIcon from '../../components/SVG/EmailIcon';
+import LinkIcon from '../../components/SVG/LinkIcon';
 import styles from './styles.module.scss';
 
 const Contact = () => {
@@ -12,7 +13,7 @@ const Contact = () => {
   const [message, setMessage] = useState('');
 
   return (
-    <section className='pt-24 px-4 pb-24' id='contact'>
+    <section className='px-4 pt-24 pb-24' id='contact'>
       <div className='container flex flex-col lg:flex-row lg:w-full'>
         <div className='lg:w-1/2'>
           <h2 className='text-[2rem] font-bold mb-4 leading-none text-text-dark md:text-5xl'>
@@ -32,11 +33,12 @@ const Contact = () => {
             </div>
             <div className='text-text-light'>
               <p className='font-bold'>REPLY TIME</p>
-              <p className='font-bold text-xl mb-1'>Within 24 hours</p>
+              <p className='mb-1 text-xl font-bold'>Within 24 hours</p>
               <a
                 className='flex items-center transition-color hover:text-primary'
-                href='mailto:zfnori@gmail.com'>
-                <span className='text-primary mr-1 flex items-center'>
+                href='mailto:zfnori@gmail.com'
+              >
+                <span className='flex items-center mr-1 text-primary'>
                   <EmailIcon />
                 </span>
                 <span>zfnori@gmail.com</span>
@@ -50,10 +52,12 @@ const Contact = () => {
             method='POST'
             encType='application/x-www-form-urlencoded'
             data-netlify='true'
-            className='w-full relative'>
+            className='relative w-full'
+          >
             <input type='hidden' name='form-name' value='contact' />
             <div
-              className={`${styles.formGroup + (name && ' ' + styles.active)}`}>
+              className={`${styles.formGroup + (name && ' ' + styles.active)}`}
+            >
               <input
                 type='text'
                 id='name'
@@ -67,9 +71,8 @@ const Contact = () => {
               <label htmlFor='name'>Name</label>
             </div>
             <div
-              className={`${
-                styles.formGroup + (email && ' ' + styles.active)
-              }`}>
+              className={`${styles.formGroup + (email && ' ' + styles.active)}`}
+            >
               <input
                 type='email'
                 id='email'
@@ -84,7 +87,8 @@ const Contact = () => {
             <div
               className={`${
                 styles.formGroup + (message && ' ' + styles.active)
-              }`}>
+              }`}
+            >
               <textarea
                 id='message'
                 name='message'
@@ -96,7 +100,20 @@ const Contact = () => {
               <span></span>
               <label htmlFor='message'>Message</label>
             </div>
-            <Button label='Submit' />
+            <div className='flex flex-col items-center justify-between md:flex-row'>
+              <Button label='Submit' />
+              <a
+                href='https://www.upwork.com/freelancers/~013e5420f61f88d376'
+                className='inline-flex items-center mt-4 group text-primary md:mt-0'
+                target='_blank'
+                rel='noreferrer'
+              >
+                You can also find me on Upwork
+                <span className='flex items-center ml-2 transform transition-trans group-hover:translate-x-1'>
+                  <LinkIcon />
+                </span>
+              </a>
+            </div>
           </form>
         </div>
       </div>
